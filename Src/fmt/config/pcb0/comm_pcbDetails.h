1c97378f25f3fd59c30471a4a01ccc079402c85b
mthielvoldt <mthielvoldt@gmail.com>
2025-08-09 18:54:32 -0400

    choose transport in comm_pcbDetails.h

---
+++ Src/fmt/config/pcb0/comm_pcbDetails.h
@@ -0,0 +1,9 @@
+#ifndef comm_pcbDetails_H
+#define comm_pcbDetails_H
+
+#define FMT_USES_UART 1
+#define RTE_Drivers_USART // Turn on the CMSIS USART driver
+#include "uart_pcbDetails.h"
+
+
+#endif // comm_pcbDetails_H
