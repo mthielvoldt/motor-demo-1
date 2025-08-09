9b98e9ecf3e2a38dd8e9bc60b55e6c5961abc76c
mthielvoldt <mthielvoldt@gmail.com>
2025-08-09 17:55:30 -0400

    add mcuCompileOptions.cmake
    specifies MCU_FAMILY, MCU_VARIANT
    adds compile definition for mcu used

---
+++ config/mcuCompileOptions.cmake
@@ -0,0 +1,10 @@
+## This file sets variables that are determined by MCU selection.
+
+if(PCB STREQUAL "0")
+  set(MCU_FAMILY "stm32g4")
+  set(MCU_VARIANT "stm32g431")
+  add_compile_definitions("STM32G431xx")
+endif()
+
+message(STATUS "PCB: ${PCB}")
+message(STATUS "MCU: ${MCU_VARIANT}")
