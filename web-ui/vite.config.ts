2c2d964757100a590c8785faa3b58e1ce7f3ca55
mthielvoldt <mthielvoldt@gmail.com>
2025-08-27 17:45:00 -0400

    debug customer app on pages 1

--- web-ui/vite.config.ts
+++ web-ui/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         eng: resolve(__dirname, "eng/index.html"),
+        main: resolve(__dirname, "index.html")
       }
     }
   }
